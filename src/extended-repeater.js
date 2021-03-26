473907e34c8525ab73f03c214642abb6b9e450b8
Dmitry Buravkin <buravkin.08@mail.ru>
2021-03-26 12:30:55 +0300

    fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  // let repeatTimes = options.repeatTimes;
+   
+  //   let addition =  String(options.addition);
+  //   let additionRepeatTimes = options.additionRepeatTimes;
+  //   let additionSeparator = options.additionSeparator;
+
+  //   let arrStr = [];
+  //   let additionArr = [];
+
+    
+
+  //   str = String(str)
+  //  let separator = options.separator;
+    
+  //   if(separator === undefined) separator = '+'
+    
+  //   if(additionSeparator === undefined) additionSeparator = '|'
+
+  //   if(repeatTimes !== undefined){
+      
+     
+  //       if(additionRepeatTimes !== undefined){
+          
+  //         for(let i = 0; i <= additionRepeatTimes - 1; i++){
+              
+
+  //               additionArr.push(addition);
+  //               additionArr.push(additionSeparator);
+  //       }
+  //        additionArr.pop();
+  //        additionArr = additionArr.join('');
+        
+  //       } 
+          
+            
+
+  //           for(let j = 0; j <= repeatTimes-1; j++){
+
+  //               arrStr.push(str);
+  //               if(additionArr.length !== 0) arrStr.push(additionArr);
+  //               arrStr.push(separator);
+
+                
+  //           }
+  //           arrStr.pop();
+  //           arrStr = arrStr.join('');
+  //           return arrStr;
+  //   } else {
+  //     arrStr.push(str);
+  //     arrStr.push(addition);
+  //     arrStr = arrStr.join('');
+  //     return arrStr;
+  //   } 
 };
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  
+  let array = arr.slice(0);
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]){
+   case '--discard-next': 
+      array[i] = undefined;
+      if(i+1 < array.length) {
+        array[i+1] = undefined;
+      }
+      break
+
+    case '--discard-prev': 
+      array[i]= undefined;
+      if(i-1 > 0) {
+        array[i-1] =undefined;
+      }
+      break
+
+    case '--double-next' :
+      if(i+1 < array.length) {
+        array[i] = array[i+1];
+      } else {
+        array[i] = undefined;
+      }
+      break
+
+    case '--double-prev' :
+      if(i-1 > 0) {
+        array[i] = array[i-1];
+      } else {
+        array[i] = undefined;
+      }
+      break
+    } 
+  
+    }
+  
+    for(let i=0;i<array.length;i++) {
+      if(array[i] === undefined) {
+        array.splice(i,1);
+        i--;
+      }
+    }
+  return array;
 };
